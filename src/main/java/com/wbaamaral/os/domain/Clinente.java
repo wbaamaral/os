a188f6ad9cfab089fa9d21816fe7d6ec5c09b5e2
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2025-06-03 19:49:27 -0400

    Normalizando finais de linha com .gitattributes

---
+++ src/main/java/com/wbaamaral/os/domain/Clinente.java
@@ -0,0 +1,13 @@
+package com.wbaamaral.os.domain;
+
+public class Clinente extends Pessoa {
+
+	public Clinente() {
+		super();
+	}
+
+	public Clinente(Long id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+	}
+
+}
---
+++ src/main/java/com/wbaamaral/os/domain/Tecnico.java
@@ -0,0 +1,14 @@
+package com.wbaamaral.os.domain;
+
+public class Tecnico extends Pessoa {
+
+	public Tecnico() {
+		super();
+	}
+
+	public Tecnico(Long id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+	}
+
+	
+}
