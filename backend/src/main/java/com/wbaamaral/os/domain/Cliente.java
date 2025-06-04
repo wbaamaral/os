e24da904f9164213de44baf3cd51c57b328ea43c
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2025-06-03 22:10:26 -0400

    Correção nome de classe

---
+++ backend/src/main/java/com/wbaamaral/os/domain/Cliente.java
@@ -0,0 +1,13 @@
+package com.wbaamaral.os.domain;
+
+public class Cliente extends Pessoa {
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Long id, String nome, String cpf, String telefone) {
+		super(id, nome, cpf, telefone);
+	}
+
+}
