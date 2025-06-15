078b9663d66233bf4992435562d28f8f10977260
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2025-06-15 18:29:24 -0400

    Aula 24 - incluir os

--- backend/src/main/java/com/wbaamaral/os/resources/OsResource.java
+++ backend/src/main/java/com/wbaamaral/os/resources/OsResource.java
@@ -1,5 +1,6 @@
 package com.wbaamaral.os.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,11 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wbaamaral.os.dtos.OsDTO;
 import com.wbaamaral.os.services.OsService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/os")
@@ -27,12 +33,23 @@ public class OsResource {
 
 		return ResponseEntity.ok().body(obj);
 	}
-	
+
 	@GetMapping
-	public ResponseEntity<List<OsDTO>> findAll(){
+	public ResponseEntity<List<OsDTO>> findAll() {
 
 		List<OsDTO> list = service.findAll().stream().map(obj -> new OsDTO(obj)).collect(Collectors.toList());
-		
+
 		return ResponseEntity.ok().body(list);
 	}
+
+	@PostMapping
+	public ResponseEntity<OsDTO> create(@Valid @RequestBody OsDTO obj) {
+
+		obj = new OsDTO(service.create(obj));
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+	}
+
 }
--- backend/src/main/java/com/wbaamaral/os/services/OsService.java
+++ backend/src/main/java/com/wbaamaral/os/services/OsService.java
@@ -6,9 +6,16 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.wbaamaral.os.domain.Cliente;
 import com.wbaamaral.os.domain.Os;
+import com.wbaamaral.os.domain.Tecnico;
+import com.wbaamaral.os.domain.enuns.Prioridade;
+import com.wbaamaral.os.domain.enuns.Status;
+import com.wbaamaral.os.dtos.OsDTO;
 import com.wbaamaral.os.repositores.OsRepository;
 import com.wbaamaral.os.services.exceptions.ObjectNotFoundException;
+
+import jakarta.validation.Valid;
 
 @Service
 public class OsService {
@@ -16,13 +23,41 @@ public class OsService {
 	@Autowired
 	private OsRepository repository;
 
+	@Autowired
+	private ClienteService clienteSrv;
+
+	@Autowired
+	private TecnicoService tecnicoSrv;
+
+	private Os fromDTO(@Valid OsDTO obj) {
+		Os ordemServico = new Os();
+		ordemServico.setObservacoes(obj.getObservacoes());
+		ordemServico.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		ordemServico.setStatus(Status.toEnum(obj.getStatus()));
+
+		Tecnico tec = tecnicoSrv.findById(obj.getTecnico());
+		Cliente cli = clienteSrv.findById(obj.getCliente());
+
+		ordemServico.setCliente(cli);
+		ordemServico.setTecnico(tec);
+
+		return ordemServico;
+	}
+
 	public Os findById(Long id) {
 		Optional<Os> obj = repository.findById(id);
 
 		return obj.orElseThrow(() -> new ObjectNotFoundException("OS não encontrada! ID: " + id + Os.class.getName()));
 	}
-	
-	public List<Os> findAll(){
+
+	public List<Os> findAll() {
 		return repository.findAll();
 	}
+
+	public Os create(@Valid OsDTO obj) {
+		Os saveOS = fromDTO(obj);
+
+		return repository.save(saveOS);
+	}
+
 }
