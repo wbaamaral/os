606ec814c4b2df3ce6d63d11a0280a84cd969e77
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2025-06-20 22:40:01 -0400

    ajustes iniciais projeto front

--- front/os/src/app/app.component.ts
+++ front/os/src/app/app.component.ts
@@ -2,14 +2,9 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root',
   imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: [],
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   title = 'os';
